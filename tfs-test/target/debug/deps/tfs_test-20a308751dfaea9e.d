208abb29ef5f85e091ef506b2122894594d3e1d7
zq <2689605766@qq.com>
2024-02-03 15:11:27 +0800

    rm target

--- tfs-test/target/debug/deps/tfs_test-20a308751dfaea9e.d
+++
@@ -1,5 +0,0 @@
-/home/zq/contents/objs/tiny-fs/tfs-test/target/debug/deps/libtfs_test-20a308751dfaea9e.rmeta: src/main.rs
-
-/home/zq/contents/objs/tiny-fs/tfs-test/target/debug/deps/tfs_test-20a308751dfaea9e.d: src/main.rs
-
-src/main.rs:
--- tfs/target/debug/deps/spin-efd988df812a95c0.d
+++
@@ -1,8 +0,0 @@
-/home/zq/contents/objs/tiny-fs/tfs/target/debug/deps/libspin-efd988df812a95c0.rmeta: /home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/lib.rs /home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/mutex.rs /home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/rw_lock.rs /home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/once.rs
-
-/home/zq/contents/objs/tiny-fs/tfs/target/debug/deps/spin-efd988df812a95c0.d: /home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/lib.rs /home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/mutex.rs /home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/rw_lock.rs /home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/once.rs
-
-/home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/lib.rs:
-/home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/mutex.rs:
-/home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/rw_lock.rs:
-/home/zq/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/once.rs:
